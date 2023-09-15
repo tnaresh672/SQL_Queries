a98090787c019680422c309f556cb5192f10c072
tnaresh672 <63588660+tnaresh672@users.noreply.github.com>
2023-09-15 21:28:23 +0530

    Query_To_Find_Table_And_Indexes_Size.sql
    Below query is to find out the total table size, indexes size.

---
+++ Query_To_Find_Table_And_Indexes_Size.sql
@@ -0,0 +1,15 @@
+/*
+Below query is to find out the total table size,indexes size.
+*/
+SELECT 
+    i.relname "Table Name",
+    indexrelname "Index Name",
+    pg_size_pretty(pg_total_relation_size(relid)) As "Total Size",
+    pg_size_pretty(pg_indexes_size(relid)) as "Total Size of all Indexes",
+    pg_size_pretty(pg_relation_size(relid)) as "Table Size",
+    pg_size_pretty(pg_relation_size(indexrelid)) "Index Size",
+    reltuples::bigint "Estimated table row count"
+ FROM pg_stat_all_indexes i 
+ JOIN pg_class c 
+   ON i.relid=c.oid 
+ WHERE i.relname='your_table';
